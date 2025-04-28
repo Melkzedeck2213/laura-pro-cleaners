1a964b5453bc469419eaeb3443f0aaed8e08b72e
Meshack2213 <meshackzkl@gmail.com>
2025-04-28 14:47:12 +0300

    layout

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,11 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from '@/context/AuthContext'
 
+import { Metadata } from 'next'
+
+
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -12,9 +17,9 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
-  title: "Laura pro cleaners",
-  description: "Dedicated cleaning company",
+export const metadata= {
+  title: "Laura Pro Cleaners | Trusted Cleaning Services",
+  description: "Professional residential and commercial cleaning services you can trust.",
 };
 
 export default function RootLayout({ children }) {
